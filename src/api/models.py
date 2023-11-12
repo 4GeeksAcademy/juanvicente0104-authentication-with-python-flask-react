baeaa4224ec9ed845c03a64d9031656ca7615339
Juan Guerrero <juanvicente0104@gmail.com>
2023-11-12 20:14:16 +0000

    Backend ready

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
+
+class UserGender(Enum):
+    male = 'male',
+    female = 'female',
+    other = 'other'
+
+class UserRol(Enum):
+    admin='admin',
+    general='general'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
+    gender = db.Column(db.Enum(UserGender), unique=False, nullable=False) #Only we want to take male, female or other
+    rol = db.Column(db.Enum(UserRol),unique=False,nullable=False, default="general")
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -15,5 +28,9 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "rol": self.rol,
+            "gender": self.gender
             # do not serialize the password, its a security breach
         }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,13 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -18,5 +15,62 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+@api.route('/signup',methods=['POST'])
+def register_user():
+    data = request.json
+    user_email = data.get("email")
+    user_username = data.get("username")
+    user_password = data.get("password")
+    user_gender = data.get("gender")
+    user_rol = data.get("rol")
+    user_active = data.get("is_active")
+
+    if user_email is None or user_username is None or user_password is None or user_gender is None or user_rol is None or user_active is None:
+        return jsonify({"msg":"missing data"}), 400
+
+    check_email = User.query.filter_by(email=user_email).first()
+
+    if check_email is not None:
+        return jsonify({"msg":"User already exists"}), 400
+
+    user_password = generate_password_hash(user_password)
+    new_user = User(email=user_email, username=user_username, password=user_password, gender=user_gender, rol=user_rol, is_active=user_active)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg":"user created succesfully!"}), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"msg":f"{error}"}), 500
+
+@api.route('/login', methods=['POST'])
+def login_user():
+    data = request.json
+    data_email = data.get("email")
+    data_password = data.get("password")
+
+    if data_email is None or data_password is None:
+        return jsonify({"msg":"missing data"}), 400
+
+    user = User.query.filter_by(email=data_email).one_or_none()
+
+    if user is None:
+        return jsonify({"msg":"wrong user or password"}), 400
+    else:
+        if check_password_hash(user.password, data_password):
+            #create a token
+            access_token = create_access_token(identity=data_email)
+            return jsonify({"token":access_token}), 200
+        else:
+            return jsonify({"msg":"wrong user or password"}), 400
+        
+@api.route('/private',methods=['GET'])
+@jwt_required()
+def authenticate_user():
+    current_user = get_jwt_identity()
+    return jsonify({"msg":current_user})
+
+    
